73a1a0a961dfcd9c77b182614751eee1af1929f2
Ryan Lee <drdgvhbh@gmail.com>
2019-08-06 07:32:00 -0400

    Add Parse Identifier

--- src/pragma_directive.rs
+++ src/pragma_directive.rs
@@ -1,7 +1,8 @@
-use crate::token::Token;
-use nom::{IResult, alt, is_a , named, tag};
+use nom::{IResult, named, tag, take_while};
 use nom::combinator::{ map };
+use nom::character::is_alphanumeric;
 use nom::error::{VerboseError, context};
+use std::str::from_utf8;
 /*
 #[derive(Debug)]
 pub struct PragmaDirective {
@@ -12,24 +13,41 @@ pub struct PragmaDirective {
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Atom {
-    Keyword(String)
+    Keyword(String),
+    Identifier(String)
 }
 
-fn parse_pragma_token(i: &str) -> IResult<&str, Atom, VerboseError<&str>> {
+fn parse_pragma_token(i: &[u8]) -> IResult<&[u8], Atom, VerboseError<&str>> {
     map(context("keyword", |s: &str|tag!(s, "pragma")), |pragma: &str| {
         Atom::Keyword(pragma.to_string())
     })(i)
+}
+
+fn parse_identifier<'a>(i: &[u8]) -> IResult<&[u8], Atom> {
+    named!(alphanum, take_while!(is_alphanumeric));
+    map(|j|alphanum(j),
+        |s| Atom::Identifier(from_utf8(s).unwrap().to_string()))(i)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use pretty_assertions::{assert_eq};
+
     #[test]
     fn parses_pragma_token() {
         let input = "pragma solidity ^0.5.6;";
         assert_eq!(
-            parse_pragma_token(input).ok(),
-            Some((" solidity ^0.5.6;", Atom::Keyword("pragma".to_string()))))
+            parse_pragma_token(input.as_bytes()).ok(),
+            Some((" solidity ^0.5.6;".as_bytes(), Atom::Keyword("pragma".to_string()))))
+    }
+
+    #[test]
+    fn parses_identifier() {
+        let input = "solidity ^0.5.6;";
+        assert_eq!(
+            parse_identifier(input.as_bytes()).ok(),
+            Some((" ^0.5.6;".as_bytes(), Atom::Identifier("solidity".to_string()))))
     }
 }
