823dcb77632161da55b63da65ed8b8ebf9a01a6f
Ryan Lee <drdgvhbh@gmail.com>
2019-08-05 20:56:59 -0400

    Add basic test for checking bytecode output

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+
+pub fn compile(assem_code: &str) -> &str {
+    return "";
 }
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use itertools::Itertools;
+    use crate::compile;
+
+    use pretty_assertions::{assert_eq, assert_ne};
+
+    #[test]
+    fn generates_bytecode_for_testfile_1() {
+        let contract = fs::read_to_string("testfile1.sol")
+            .expect("failed to read testfile1.sol");
+        let expected_bytecode = fs::read_to_string("testfile1.bin")
+            .expect("failed to read testfile1.sol");
+        let byte_code = compile(&contract);
+        assert_eq!(byte_code, expected_bytecode)
+    }
 }
