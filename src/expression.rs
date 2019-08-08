3a874c11aff32929e6e810d4599b4548a5063841
Ryan Lee <drdgvhbh@gmail.com>
2019-08-08 08:00:49 -0400

    dumb

--- src/expression.rs
+++ src/expression.rs
@@ -3,8 +3,9 @@ use crate::elementary_type_name::{parse as parse_elementary_type_name, Elementar
 use crate::storage_location::{parse as parse_storage_location, StorageLocation};
 use nom::{
     branch::alt,
+    bytes::complete::take,
     character::complete::{char, multispace0, multispace1},
-    combinator::{map, opt},
+    combinator::{flat_map, map, opt},
     multi::separated_nonempty_list,
     sequence::{delimited, preceded, tuple},
     IResult,
@@ -19,7 +20,7 @@ pub enum ContractDefinition {
     Interface,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum TypeName {
     ElementaryTypeName(ElementaryTypeName),
     UserDefinedTypeName,
@@ -38,6 +39,7 @@ pub fn parse_user_defined_type_name(i: &[u8]) -> IResult<&[u8], TypeName> {
 pub fn parse_type_name(i: &[u8]) -> IResult<&[u8], TypeName> {
     alt((
         map(parse_elementary_type_name, |e| {
+            // println!("{:#?}", TypeName::ElementaryTypeName(e.clone()));
             TypeName::ElementaryTypeName(e)
         }),
         parse_user_defined_type_name,
@@ -52,23 +54,39 @@ pub struct Parameter {
 }
 
 pub fn parse_parameter(i: &[u8]) -> IResult<&[u8], Box<Parameter>> {
-    map(
-        tuple((
-            parse_type_name,
-            opt(preceded(multispace1, parse_storage_location)),
-            opt(preceded(multispace1, parse_identifier)),
-        )),
-        |s| {
-            let (typename, storage_location, identifier) = s;
-            Box::new(Parameter {
-                typename,
-                storage_location,
-                identifier: identifier.map(|id| id.to_string()),
-            })
-        },
-    )(i)
+    flat_map(parse_type_name, |typename| {
+        alt((
+            map(
+                tuple((
+                    preceded(multispace1, parse_storage_location),
+                    preceded(multispace1, parse_identifier),
+                )),
+                move |tup| {
+                    let (storage, id) = tup;
+                    Box::new(Parameter {
+                        typename: typename,
+                        storage_location: Some(storage),
+                        identifier: Some(id.to_string()),
+                    })
+                },
+            ),
+            map(preceded(multispace1, parse_identifier), move |id| {
+                Box::new(Parameter {
+                    typename: typename,
+                    storage_location: None,
+                    identifier: Some(id.to_string()),
+                })
+            }),
+            map(multispace0, move |_| {
+                Box::new(Parameter {
+                    typename: typename,
+                    storage_location: None,
+                    identifier: None,
+                })
+            }),
+        ))
+    })(i)
 }
-
 fn parse_interface_expression(i: &[u8]) -> IResult<&[u8], ContractDefinition> {
     map(
         delimited(
@@ -136,6 +154,23 @@ mod tests {
                     typename: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
                     storage_location: None,
                     identifier: Some("isWorking".to_string()),
+                })
+            )
+        )
+    }
+
+    #[test]
+    fn parses_parameter_with_type_only() {
+        let input = "bool   \n";
+        let (remaining, param) = parse_parameter(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), param),
+            (
+                "",
+                Box::new(Parameter {
+                    typename: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
+                    storage_location: None,
+                    identifier: None,
                 })
             )
         )
