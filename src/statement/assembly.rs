7da01087b6e7e5862db32ca42b32923e9967f84d
Ryan Lee <drdgvhbh@gmail.com>
2019-08-08 21:40:18 -0400

    Add Assembly Identifier List

--- src/statement/assembly.rs
+++ src/statement/assembly.rs
@@ -1,6 +1,36 @@
-use nom::{IResult, multi::{separated_nonempty_list}};
-use crate::atom::{parse_identifier};
+use crate::atom::parse_identifier;
+use nom::{
+    character::complete::{char, multispace0},
+    multi::separated_nonempty_list,
+    sequence::preceded,
+    IResult,
+};
 
 fn parse_assembly_identifier_list(i: &[u8]) -> IResult<&[u8], Vec<String>> {
-    separated_nonempty_list(char(','), parse_identifier)(i)
+    separated_nonempty_list(char(','), preceded(multispace0, parse_identifier))(i)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::str::from_utf8;
+
+    #[test]
+    fn parses_assem_identifier_list() {
+        let input = "a,    b,      result\n";
+        let result = parse_assembly_identifier_list(input.as_bytes());
+        if result.is_err() {
+            result.expect("error");
+        } else {
+            let (remaining, identifiers) = result.ok().unwrap();
+            assert_eq!(
+                (from_utf8(remaining).unwrap(), identifiers),
+                (
+                    "\n",
+                    vec!["a".to_string(), "b".to_string(), "result".to_string()]
+                )
+            )
+        }
+    }
 }
