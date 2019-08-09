e1ebde37aab6d229fa730ffb1df604a88a4e2d2e
Ryan Lee <drdgvhbh@gmail.com>
2019-08-09 00:29:13 -0400

    Add Hex Number parser

--- src/literal/mod.rs
+++ src/literal/mod.rs
@@ -1,4 +1,10 @@
-use nom::{character::complete::digit1, combinator::map, multi::many1, sequence::tuple, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, hex_digit1},
+    combinator::map,
+    sequence::preceded,
+    IResult,
+};
 use std::str::from_utf8;
 
 mod boolean;
@@ -7,12 +13,21 @@ mod number_unit;
 #[derive(Debug, PartialEq, Clone)]
 pub enum Literal {
     DecimalNumber(String),
+    HexNumber(String),
 }
 
 // TODO: support more than integer
 fn parse_decimal_number(i: &[u8]) -> IResult<&[u8], Literal> {
     map(digit1, |d| {
         Literal::DecimalNumber(from_utf8(d).ok().unwrap().to_string())
+    })(i)
+}
+
+fn parse_hex_number(i: &[u8]) -> IResult<&[u8], Literal> {
+    map(preceded(tag("0x"), hex_digit1), |s| {
+        let mut num = from_utf8(s).ok().unwrap().to_string();
+        num.insert_str(0, "0x");
+        Literal::HexNumber(num)
     })(i)
 }
 
@@ -31,6 +46,24 @@ mod tests {
             assert_eq!(
                 (from_utf8(remaining).unwrap(), b),
                 ("\n", Literal::DecimalNumber("323".to_string()))
+            )
+        }
+    }
+
+    #[test]
+    fn parses_hex_number() {
+        let input = b"0xFB88dE099e13c3ED21F80a7a1E49f8CAEcF10df6\n";
+        let result = parse_hex_number(input);
+        if result.is_err() {
+            result.expect("should parse break");
+        } else {
+            let (remaining, b) = result.ok().unwrap();
+            assert_eq!(
+                (from_utf8(remaining).unwrap(), b),
+                (
+                    "\n",
+                    Literal::HexNumber("0xFB88dE099e13c3ED21F80a7a1E49f8CAEcF10df6".to_string())
+                )
             )
         }
     }
