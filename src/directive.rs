820af60aaf26c4b0820c281eea94322862a49bd6
Ryan Lee <drdgvhbh@gmail.com>
2019-08-08 21:11:27 -0400

    Rename statement module to directive module

--- src/directive.rs
+++ src/directive.rs
@@ -14,7 +14,7 @@ pub struct PragmaDirective {
   pub value: String,
 }
 
-fn parse_pragma_statement(i: &[u8]) -> IResult<&[u8], Box<PragmaDirective>> {
+fn parse_pragma_directive(i: &[u8]) -> IResult<&[u8], Box<PragmaDirective>> {
   named!(tuple<&[u8], (Atom, Atom, Atom, Delimiter)>, ws!(tuple!(
         parse_pragma, parse_identifier, parse_anything_till_semi, parse_semicolon)));
   map(tuple, |t| {
@@ -34,9 +34,9 @@ mod tests {
   use pretty_assertions::assert_eq;
 
   #[test]
-  fn parses_pragma_statement() {
+  fn parses_pragma_directive() {
     let input = "pragma solidity ^0.5.6;";
-    let (remaining, directive) = parse_pragma_statement(input.as_bytes()).ok().unwrap();
+    let (remaining, directive) = parse_pragma_directive(input.as_bytes()).ok().unwrap();
     assert_eq!(
       (from_utf8(remaining).unwrap(), directive.as_ref()),
       (
