40b6cfbbeb313fa9614bf01e10252eee678c83a7
Ryan Lee <drdgvhbh@gmail.com>
2019-08-11 16:30:03 -0400

    Add parse statement

--- src/statement/mod.rs
+++ src/statement/mod.rs
@@ -5,12 +5,14 @@ use crate::{
 };
 use nom::{
     branch::alt,
+    bytes::streaming::tag,
     character::complete::{char, multispace0, multispace1},
-    combinator::{map, opt},
+    combinator::{complete, map, opt},
     multi::{many0, separated_list},
-    sequence::{delimited, preceded, tuple},
+    sequence::{delimited, preceded, terminated, tuple},
     IResult,
 };
+use std::str::from_utf8;
 
 mod assembly;
 
@@ -90,22 +92,43 @@ pub enum Statement {
 
 pub fn parse_statement(i: &[u8]) -> IResult<&[u8], Statement> {
     alt((
-        map(parse_block, |x| Statement::Block(x)),
-        map(parse_expression, |x| Statement::Expression(x)),
-        map(parse_variable_declaration, |x| {
-            Statement::VariableDeclaration(x)
-        }),
-        map(parse_variable_definition, |x| {
-            Statement::VariableDefinition(x)
-        }),
+        alt((
+            map(parse_block, |x| Statement::Block(x)),
+            terminated(
+                alt((
+                    map(parse_expression, |x| Statement::Expression(x)),
+                    map(parse_variable_declaration, |x| {
+                        Statement::VariableDeclaration(x)
+                    }),
+                    map(parse_variable_definition, |x| {
+                        Statement::VariableDefinition(x)
+                    }),
+                )),
+                preceded(multispace0, tag(";")),
+            ),
+        )),
+        alt((
+            map(parse_block, |x| Statement::Block(x)),
+            terminated(
+                alt((
+                    map(parse_variable_declaration, |x| {
+                        Statement::VariableDeclaration(x)
+                    }),
+                    map(parse_variable_definition, |x| {
+                        Statement::VariableDefinition(x)
+                    }),
+                )),
+                preceded(multispace0, tag(";")),
+            ),
+        )),
     ))(i)
 }
 
 pub fn parse_block(i: &[u8]) -> IResult<&[u8], Vec<Statement>> {
     delimited(
-        preceded(multispace0, char('{')),
+        preceded(multispace0, tag("{")),
         many0(preceded(multispace0, parse_statement)),
-        preceded(multispace0, char('}')),
+        preceded(multispace0, tag("}")),
     )(i)
 }
 
@@ -142,6 +165,28 @@ mod tests {
     }
 
     #[test]
+    fn parses_with_no_storage_declaration() {
+        let input = b"     bool            mahmemory";
+        let result = parse_variable_declaration(input);
+        if result.is_err() {
+            result.expect("should parse variable declaration");
+        } else {
+            let (remaining, decl) = result.ok().unwrap();
+            assert_eq!(
+                (from_utf8(remaining).unwrap(), decl),
+                (
+                    "",
+                    VariableDeclaration {
+                        type_name: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
+                        storage_location: None,
+                        identifier: "mahmemory".to_string(),
+                    }
+                )
+            )
+        }
+    }
+
+    #[test]
     fn parses_variable_definition() {
         let input = b"    (bool     memory     a,     bool     memory    b   ) =    true";
         let result = parse_variable_definition(input);
@@ -170,6 +215,62 @@ mod tests {
                             Boolean::True
                         ))
                     }
+                )
+            )
+        }
+    }
+
+    #[test]
+    fn parses_block() {
+        let input = b"{(bool     memory     a,     bool     memory    b   ) =    true;}";
+        let result = parse_block(input);
+        if result.is_err() {
+            result.expect("should parse block");
+        } else {
+            let (remaining, decl) = result.ok().unwrap();
+            assert_eq!(
+                (from_utf8(remaining).unwrap(), decl),
+                (
+                    "",
+                    vec![Statement::VariableDefinition(VariableDefinition {
+                        declarations: vec![
+                            VariableDeclaration {
+                                type_name: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
+                                storage_location: Some(StorageLocation::Memory),
+                                identifier: "a".to_string(),
+                            },
+                            VariableDeclaration {
+                                type_name: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
+                                storage_location: Some(StorageLocation::Memory),
+                                identifier: "b".to_string(),
+                            }
+                        ],
+                        rhs: Expression::PrimaryExpression(PrimaryExpression::BooleanLiteral(
+                            Boolean::True
+                        ))
+                    })]
+                )
+            )
+        }
+    }
+
+    #[test]
+    fn parses_block2() {
+        let input = b"{bool memory a;}";
+        let result = parse_block(input);
+        if result.is_err() {
+            result.expect("should parse block2");
+        } else {
+            let (remaining, decl) = result.ok().unwrap();
+            assert_eq!(
+                (from_utf8(remaining).unwrap(), decl),
+                (
+                    "",
+                    vec![Statement::VariableDeclaration(VariableDeclaration {
+                        type_name: TypeName::ElementaryTypeName(ElementaryTypeName::Bool),
+                        storage_location: Some(StorageLocation::Memory),
+                        identifier: "a".to_string(),
+                    })]
                 )
             )
         }
