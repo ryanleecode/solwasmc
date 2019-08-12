2dbac8d7341d2426cf27839494fe4253ff14142c
Ryan Lee <drdgvhbh@gmail.com>
2019-08-12 18:28:41 -0400

    Fix elementary type name bugs

--- src/elementary_type_name/mod.rs
+++ src/elementary_type_name/mod.rs
@@ -1,139 +1,137 @@
-// GENERATED: DO NOT EDIT
-use nom::{
-  named,
-  tag,
-  IResult,
-  combinator::{map},
-  branch::{alt}
-};
-pub use crate::elementary_type_name::uint::{UInt};
-use crate::elementary_type_name::uint::{parse as parse_uInt};
+use crate::elementary_type_name::uint::parse as parse_uInt;
+pub use crate::elementary_type_name::uint::UInt;
+use nom::{branch::alt, combinator::map, named, tag, IResult};
 
 mod uint;
 
 const ADDRESS: &str = r#"address"#;
 const BOOL: &str = r#"bool"#;
 const STRING: &str = r#"string"#;
-const INT: &str = r#"Int"#;
-const UINT: &str = r#"Uint"#;
-const BYTE: &str = r#"Byte"#;
-const FIXED: &str = r#"Fixed"#;
-const UFIXED: &str = r#"Ufixed"#;
+const INT: &str = r#"int"#;
+const BYTE: &str = r#"byte"#;
+const FIXED: &str = r#"fixed"#;
+const UFIXED: &str = r#"ufixed"#;
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum ElementaryTypeName {
-	Address,
-	Bool,
-	String,
-	Int,
-	UInt(UInt),
-	Byte,
-	Fixed,
-	Ufixed,
+    Address,
+    Bool,
+    String,
+    Int,
+    UInt(UInt),
+    Byte,
+    Fixed,
+    Ufixed,
 }
 
 fn parse_address(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"address"#));
-  map(semi, |_| ElementaryTypeName::Address)(i)
+    named!(semi, tag!(ADDRESS));
+    map(semi, |_| ElementaryTypeName::Address)(i)
 }
 fn parse_bool(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"bool"#));
-  map(semi, |_| ElementaryTypeName::Bool)(i)
+    named!(semi, tag!(BOOL));
+    map(semi, |_| ElementaryTypeName::Bool)(i)
 }
 fn parse_string(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"string"#));
-  map(semi, |_| ElementaryTypeName::String)(i)
+    named!(semi, tag!(STRING));
+    map(semi, |_| ElementaryTypeName::String)(i)
 }
 fn parse_int(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"Int"#));
-  map(semi, |_| ElementaryTypeName::Int)(i)
+    named!(semi, tag!(INT));
+    map(semi, |_| ElementaryTypeName::Int)(i)
 }
-
 fn parse_byte(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"Byte"#));
-  map(semi, |_| ElementaryTypeName::Byte)(i)
+    named!(semi, tag!(BYTE));
+    map(semi, |_| ElementaryTypeName::Byte)(i)
 }
 fn parse_fixed(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"Fixed"#));
-  map(semi, |_| ElementaryTypeName::Fixed)(i)
+    named!(semi, tag!(FIXED));
+    map(semi, |_| ElementaryTypeName::Fixed)(i)
 }
 fn parse_ufixed(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-  named!(semi, tag!(r#"Ufixed"#));
-  map(semi, |_| ElementaryTypeName::Ufixed)(i)
+    named!(semi, tag!(UFIXED));
+    map(semi, |_| ElementaryTypeName::Ufixed)(i)
 }
 pub fn parse(i: &[u8]) -> IResult<&[u8], ElementaryTypeName> {
-	alt((
-		parse_address,
-		parse_bool,
-		parse_string,
-		parse_int,
-		map(parse_uInt, |x| ElementaryTypeName::UInt(x)),
-		parse_byte,
-		parse_fixed,
-		parse_ufixed,
-	))(i)
+    alt((
+        parse_address,
+        parse_bool,
+        parse_string,
+        parse_int,
+        map(parse_uInt, |x| ElementaryTypeName::UInt(x)),
+        parse_byte,
+        parse_fixed,
+        parse_ufixed,
+    ))(i)
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	use std::str::from_utf8;
+    use super::*;
+    use std::str::from_utf8;
 
-	#[test]
-	fn parses_address() {
-	    let input = r#"addressa"#;
-	    let (remaining, name) = parse_address(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Address))
-	}
-	#[test]
-	fn parses_bool() {
-	    let input = r#"boola"#;
-	    let (remaining, name) = parse_bool(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Bool))
-	}
-	#[test]
-	fn parses_string() {
-	    let input = r#"stringa"#;
-	    let (remaining, name) = parse_string(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::String))
-	}
-	#[test]
-	fn parses_int() {
-	    let input = r#"Inta"#;
-	    let (remaining, name) = parse_int(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Int))
-	}
+    #[test]
+    fn parses_address() {
+        let input = format!("{} a", ADDRESS);
+        let (remaining, name) = parse_address(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Address)
+        )
+    }
+    #[test]
+    fn parses_bool() {
+        let input = format!("{} a", BOOL);
+        let (remaining, name) = parse_bool(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Bool)
+        )
+    }
+    #[test]
+    fn parses_string() {
+        let input = format!("{} a", STRING);
+        let (remaining, name) = parse_string(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::String)
+        )
+    }
+    #[test]
+    fn parses_int() {
+        let input = format!("{} a", INT);
+        let (remaining, name) = parse_int(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Int)
+        )
+    }
 
-	#[test]
-	fn parses_byte() {
-	    let input = r#"Bytea"#;
-	    let (remaining, name) = parse_byte(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Byte))
-	}
-	#[test]
-	fn parses_fixed() {
-	    let input = r#"Fixeda"#;
-	    let (remaining, name) = parse_fixed(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Fixed))
-	}
-	#[test]
-	fn parses_ufixed() {
-	    let input = r#"Ufixeda"#;
-	    let (remaining, name) = parse_ufixed(input.as_bytes()).ok().unwrap();
-	    assert_eq!(
-	        (from_utf8(remaining).unwrap(), name),
-	        ("a", ElementaryTypeName::Ufixed))
-	}
+    #[test]
+    fn parses_byte() {
+        let input = format!("{} a", BYTE);
+        let (remaining, name) = parse_byte(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Byte)
+        )
+    }
+    #[test]
+    fn parses_fixed() {
+        let input = format!("{} a", FIXED);
+        let (remaining, name) = parse_fixed(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Fixed)
+        )
+    }
+    #[test]
+    fn parses_ufixed() {
+        let input = format!("{} a", UFIXED);
+        let (remaining, name) = parse_ufixed(input.as_bytes()).ok().unwrap();
+        assert_eq!(
+            (from_utf8(remaining).unwrap(), name),
+            (" a", ElementaryTypeName::Ufixed)
+        )
+    }
 }
