bfaedab8fc9b0b165e608fe2e8db54b085cef06e
Ryan Lee <drdgvhbh@gmail.com>
2019-08-12 23:47:08 -0400

    Update testfiles

--- testfile2.sol
+++ testfile2.sol
@@ -4,11 +4,21 @@ contract IdentityProxy {
 	constructor()
 		public
 	{
+        assembly {
+			sstore(0x13758194b50a616b11f5a07b8430a3e0aacf9afe73738712e244d341a949ad27, 3)
+			
+		}
 		GeneralERC20(0xf25186B5081Ff5cE73482AD761DB0eB0d25abfBF).transfer(0x821aEa9a577a9b44299B9c15c88cf3087F3b5544, 250);
 	}
 
 	function () external
 	{
 		address to = address(0xFB88dE099e13c3ED21F80a7a1E49f8CAEcF10df6);
+        assembly {
+			calldatacopy(0, 0, calldatasize())
+			let result := delegatecall(sub(gas, 10000), to, 0, calldatasize(), 0, 0)
+			returndatacopy(0, 0, returndatasize)
+			switch result case 0 {revert(0, returndatasize)} default {return (0, returndatasize)}
+		}
 	}
 }
