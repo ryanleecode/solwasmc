9cf9bed41402e5f01d81ce7ec1aa04e8bddc67ba
Ryan Lee <drdgvhbh@gmail.com>
2019-08-20 23:37:37 -0400

    Add Hex Literal

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -24,3 +24,27 @@ pub enum ElementaryTypeName {
     UInt(UInt),
     Byte(Byte),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::solidity;
+
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn parses_hex_literal_double_quotes() {
+        let parse_result = solidity::HexLiteralParser::new().parse("hex\"123456\"");
+        assert!(parse_result.is_ok());
+        let literal = parse_result.ok().unwrap();
+        assert_eq!(literal, "123456".to_string())
+    }
+
+    #[test]
+    fn parses_hex_literal_single_quotes() {
+        let parse_result = solidity::HexLiteralParser::new().parse("hex'AdEc36'");
+        assert!(parse_result.is_ok());
+        let literal = parse_result.ok().unwrap();
+        assert_eq!(literal, "AdEc36".to_string())
+    }
+}
