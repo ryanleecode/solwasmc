3c8423faa3e6c01179d4470cc2f79401c6acb1b3
Ryan Lee <drdgvhbh@gmail.com>
2019-08-20 20:08:11 -0400

    Add Postfix Expression

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,10 @@
-use crate::lexer::{ElementaryTypeName, BooleanLiteral};
+use crate::lexer::{BooleanLiteral, ElementaryTypeName, Operator};
 
 pub type ElementaryTypeNameExpression = ElementaryTypeName;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Expression {
+    PostfixExpression(Box<Expression>, Operator),
     PrimaryExpression(PrimaryExpression),
 }
 
@@ -12,4 +13,49 @@ pub enum PrimaryExpression {
     BooleanLiteral(BooleanLiteral),
     Identifier(String),
     ElementaryTypeNameExpression(ElementaryTypeNameExpression),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use pretty_assertions::assert_eq;
+
+    use crate::{
+        ast::{Expression, PrimaryExpression},
+        lexer::Operator,
+        solidity,
+    };
+
+    #[test]
+    fn parses_postfix_increment_operator() {
+        let parse_result = solidity::ExpressionParser::new().parse("a++");
+        assert!(parse_result.is_ok());
+        let expr = parse_result.ok().unwrap();
+        assert_eq!(
+            expr,
+            Box::new(Expression::PostfixExpression(
+                Box::new(Expression::PrimaryExpression(
+                    PrimaryExpression::Identifier("a".to_string())
+                )),
+                Operator::Increment,
+            ))
+        )
+    }
+
+    #[test]
+    fn parses_postfix_decrement_operator() {
+        let parse_result = solidity::ExpressionParser::new().parse("a--");
+        assert!(parse_result.is_ok());
+        let expr = parse_result.ok().unwrap();
+        assert_eq!(
+            expr,
+            Box::new(Expression::PostfixExpression(
+                Box::new(Expression::PrimaryExpression(
+                    PrimaryExpression::Identifier("a".to_string())
+                )),
+                Operator::Decrement,
+            ))
+        )
+    }
 }
